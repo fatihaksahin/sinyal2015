ce4ee4157977c7f28f0225fb3ebb66c7697fd8a3
fatihaksahin <fatihaksahin@hotmail.com.tr>
2015-12-15 00:30:36 +0200

    note.m dosyasi degistirildi

--- note.m
+++ note.m
@@ -1,4 +1,4 @@
-function [xx,tt] = note(ff,dur) %fonksiyonu tanimliyoruz.girilicek argumanlarý tanýmladýk
+function [xx,tt] = note(ff,dur) %fonksiyonu tanimliyoruz.girilicek argumanlari tanýmladýk
 tt = 0:1/(100*ff):dur; %0 dan dur a kadar 1/(100*ff) kadar artýyor
 xx = sin(2*pi*ff*tt); %bilgileri kullanarak sinüs olusturuyoruz
 %xx,tt]=note(1,3);
