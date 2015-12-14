d04c9009f6486d7875e33d261876894fe4fb9d4a
fatihaksahin <fatihaksahin@hotmail.com.tr>
2015-12-15 00:28:37 +0200

    frek.m dosyası degistirildi.

--- frek.m
+++ frek.m
@@ -1,5 +1,6 @@
 function [c]=frek(nota,m)
-
+%fonksiyon tanýmladýk ve girlicek argumanlarý belirledik.Üstte girdigimiz
+%notaya gore altta d degiskeni deger alýyor ve formul hesaplanýyor
 switch nota  %notaya karsilik gelen degeri aliyor.Örnegin do 1.sirada 1 degerini aliyor.C# 2.sirada 2 degerini aliyor
     case {'Do'}
         d=1;
