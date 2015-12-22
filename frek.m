8f2b5fefc6283d56b886aa3dbd4eba630ea9e73d
fatihaksahin <fatihaksahin@hotmail.com.tr>
2015-12-22 11:48:29 +0200

    frek.m dosyası degistirildi

--- frek.m
+++ frek.m
@@ -1,34 +1,49 @@
 function [c]=frek(nota,m)
 %fonksiyon tanimladik ve girlicek argumanlari belirledik.Üstte girdigimiz
 %notaya gore altta d degiskeni deger aliyor ve formul hesaplaniyor
+a=440;%La degerini referans aldýk
+if nargin<2
+    m=4;
+end
+    
 switch nota  %notaya karsilik gelen degeri aliyor.Örnegin do 1.sirada 1 degerini aliyor.C# 2.sirada 2 degerini aliyor
+    
     case {'Do'}
         d=1;
-        case {'C#'}
+        case {'Dod'}
         d=2;
         case {'Re'}
         d=3;
-        case {'Eb'}
+        case {'Mib'}
         d=4;
         case {'Mi'}
         d=5;
         case {'Fa'}
         d=6;
-        case {'F#'}
+        case {'Fad'}
         d=7;
         case {'Sol'}
         d=8;
-        case {'G#'}
+        case {'Sold'}
         d=9;
         case {'La'}
         d=10;
-        case {'Bb'}
+        case {'Sib'}
          d=11;
         case {'Si'}
         d=12;
+        case{'sus'}
+        a=0;
+        d=0;
+        
+      otherwise %hiçbir þey girilmesse default degeri
+        d=10;
+    
     
 end
-c=(440)*2^((m-4)+(d-10)/12);
+c=(a)*2^((m-4)+(d-10)/12);
+
+
 %la notasýnýn frekansýný temel aldým
 %http://www.darussafaka.k12.tr/wp-content/uploads/2014/04/PROJE-24-MATEMAT%C4%B0%C4%9E%C4%B0N-MELOD%C4%B0S%C4%B0-FMV-I%C5%9EIK-AYAZA%C4%9EA-II.pdf
 %formulu burdan buldum sayfa 30
