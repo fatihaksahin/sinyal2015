58def0e0c5a023a813194bb1390824d84c49ae72
fatihaksahin <fatihaksahin@hotmail.com.tr>
2015-12-29 13:30:08 +0200

    note.m guncellendi

--- note.m
+++ note.m
@@ -1,22 +1,17 @@
-function [x,t] = note(f,vurus) %fonksiyonu tanimliyoruz.girilicek argumanlari tanýmladýk
-fs=8192;%örnekleme frekansý
-t = 0:1/fs:vurus; %0 dan dur a kadar 1/(100*ff) kadar artýyor
-%xx,tt]=note(1,3);
-%plot(tt,xx)
-%grafigi cizdirmek icin ustteki kodu command window'a yazariz
-
-    elemanS = length(t); %% Eleman sayýsý
-    
-    attack = linspace(0,1.5,(elemanS*2/8));
-    
-    decay = linspace(1.5-(0.5/(elemanS/8)),1,(elemanS/8));
-    
-    sustain = ones(1,(elemanS/2));
-    
-    relase = linspace(1-(1/(elemanS/8)),0,(elemanS/8));
-    
-    zarf = [attack, decay, sustain, relase];
-
-    x = zarf .* sin(2*pi*f*t + zarf); %% zarflý sinüs 
-
-
+function[x,t]=note(frekans,vurus)% note fonksiyonu tanimladik
+    fs=8192;%ornekleme frekansý
+    t=0:1/fs:vurus-(1/fs); %sinyalin periyodu 
+       a=length(t);%periyod boyutu 
+    hk=[1 0.8 0.4 0.1]; %harmonik katsayilari
+        b=length(hk); %hk boyutu
+        x=zeros(1,a); 
+        for i=1:b %for döngusu
+            x=x+(hk(i)*sin(2*pi*(i*frekans)*t)); %harmonik olusturuldu
+        end
+A=linspace(0,1.5,a/4);%vektor
+B=linspace(1.5,1,a/8);%vektor
+C=linspace(1,1,a/2);%vektor
+D=linspace(1,0,a/8);%vektor
+ zarf=[A B C D];%vektorlerý diziye attýk
+ x=x.*zarf;%xle zarf dizisindeki elemanlarla carptik
+ 
