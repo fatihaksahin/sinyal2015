74a47205eb47b39090c5853042c16cf2843a9acd
fatihaksahin <fatihaksahin@hotmail.com.tr>
2016-01-05 11:10:23 +0200

    sentez.m degistirildi

--- sentez.m
+++ sentez.m
@@ -1,27 +1,34 @@
-[nota,b,c]=textread('notalar.txt','%s %s %s');                              %notalar.txt degerleri okur
-fs=8192;                                                                    %örnekleme frekansý
-ds=round(fs/100);                                                           %durma süresi
-go=round(fs/10);                                                            %gecikme süresi
-notalar=zeros(1,fs);                                                        %dizi olusturduk 
-a=zeros(1,length(notalar));                                                 
-durak=zeros(1,ds);                                                         
-                                                                            
-   for i=1:(length(nota))
-       oktav((i),1)=str2num(cell2mat(b(i,1)));                              %nota dizisi boyu döngüsü olusturduk
-       vurus((i),1)=str2num(cell2mat(c(i,1)));                              %convert iþlemi
-       frekans((i),1)=frek(nota((i),1),oktav((i),1));                       %frek fonk u cagýrdýk
-       [x,t]=note(frekans((i),1),vurus((i),1));                             %note fonk u cagýrdýk
-       notalar=[notalar durak x];                                           %birleþtirme iþlemi atama
-   end                                                                      
- notalarecho=zeros(1,(length(notalar)));                                    %0 lardan olusan bir dizi
- for i=1:(length(notalar))                                                  %döngü olusturduk
-     f=(go+i);                                                              %gecikme süresi döngüye sokuldu
-     if f<(length(notalar))                                                 %þart saðlandý geçimi engellemek için
-     notalarecho(i)=notalar(i)*0.3+notalar(f);                              %yüzde 30 kaydýrma iþlemi
+%%
+[nota,b,c]=textread('Notalar.txt','%s %s %s'); %notalar.txt degerlerini okuduk
+oktavdegeri=1; %oktav degeri degistirilebilir
+fs=8192;%örnekleme frekansý
+ds=round(fs/100); %durma süresi 
+go=round(fs/10); %gecikme süresi 
+notalar=zeros(1,fs); %örnekleme süresi kadar sýfýrdan  dizi oluþturuldu
+a=zeros(1,length(notalar)); %notalarýn boutu kadar sýfýrdan dizi olusturduk
+durak=zeros(1,ds); %durma süresi boyutunda sýfýrdan olusan bir dizi oluþturduk
+oktav=str2num(cell2mat(b)); %dosyadan cell olarak okunan deðiþkenler double deðere dönüþtürüldü.
+%%
+   for i=1:(length(nota)) %nota dizisi boyu kadar döngü olþturuldu
+       vurus((i),1)=str2num(cell2mat(c(i,1))); %dosyadan cell olarak okunan deðiþkenler double deðere dönüþtürüldü.
+       frekans((i),1)=frek(nota((i),1),oktav((i),1),oktavdegeri); %daha önceden oluþturulmuþ frek fonsiyonu çaðýrýldý
+       [x,t]=note(frekans((i),1),vurus((i),1)); %daha önceden olýþturulmuþ note fonksiyonu çaðýrýldý.
+       notalar=[notalar durak x]; %note fonsiyonunun döndürdüðü sinyal deðeri aralarýna daha önce oluþturulmu durak dizisi de yerleþtirilerek birleþtirildi
+   end %for döngüsü bitirildi
+   plot(notalar)
+%%
+ notalarecho=zeros(1,(length(notalar))); %notalar matrisi boyutlarýnda sýfýrdan olusan bir dizi oluþturuldu
+ for i=1:(length(notalar)) %notalar dizisi boyu kadar döngü olþturuldu
+     f=(go+i); %echo oluþturmak için gerekli olan gecikme süresi döngüye sokuldu
+     if f<(length(notalar)) %gecikme süresi notalar matrisinin boyutunu geçmesin diye þart saðlandý
+     notalarecho(i)=notalar(i)*0.3+notalar(f); %notalar matrisinin herbir deðerinin %30'u alýnýp geçikme süresi kadar kaydýrýlmýþtýr
      else 
       notalarecho(i)=notalar(i)*0.3+0; 
      end
  end 
-  normalizenotalarecho=zeros(1,(length(notalar)));                          %normalizasyon iþlemleri için deðiþken tanýmladýk
-  normalizenotalarecho=notalarecho/max(abs(notalarecho));                   %tepe degeri 1 e normalize edilmiþtir.
-sound(normalizenotalarecho)                                                 %sinyal
+ plot(notalarecho)
+%%
+  normalizenotalarecho=zeros(1,(length(notalar)));%normalizasyon iþlemi 
+  normalizenotalarecho=notalarecho/max(abs(notalarecho));%sinyalin tepe deðerleri 1'e normalize edilmiþtir.
+  plot(normalizenotalarecho)
+sound(normalizenotalarecho) %çalma iþlemi
